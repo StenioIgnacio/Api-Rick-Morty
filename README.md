1012906fb0366e728cc91581a564e39a9e5bc3d4
Stênio Ignácio <stenioignacio@gmail.com>
2023-09-24 19:50:18 -0300

    alterando praticamente tudo

---
+++ README.md
@@ -0,0 +1,10 @@
+# Curso: Analise e desenvolvimento de sistemas
+
+# Matéria: DESENVOLV DE APLICACOES P/ A INTERNET
+
+# Nome: StÊnio Ribeiro Ignácio
+# RA: 1937239
+
+# Objetivo do projeto:
+O objetivo é listar os personagens da api do Rick & Morty.
+Link da api utilizada: https://rickandmortyapi.com/api/
---
+++ src/views/HomeView.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="home">
+    <h1>Rick & Morty</h1>
+      <div class="content">
+      <div v-for="(character, index) in characters" :key="index">
+      <img :src="character.image" alt="">
+      <h1> {{character.name}} </h1>
+      <h4> {{character.gender}} </h4>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import axios from 'axios'
+
+export default {
+  name: 'HomeView',
+  components: {
+    
+  },
+  data(){
+    return{
+      characters: []
+    }
+  },
+  mounted(){
+    this.getTodos();
+  },
+  methods: {
+    getTodos(){
+      axios.get('https://rickandmortyapi.com/api/character')
+      .then(res => 
+      (this.characters = res.data.results))
+      .catch(e => {
+        console.log(e)
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.content{
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1fr;
+  gap: 0px 10px;
+}
+</style>
